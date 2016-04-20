cf6e619e01f553c483b9177a7fd7d8af6b44512a
Guille <guillevch@gmail.com>
2016-04-20 14:23:32 +0200

    gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,31 @@
 'use strict';
 
+import del from 'del';
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import del from 'del';
+import sourcemaps from 'gulp-sourcemaps';
 
 gulp.task('clean', del.bind(null, ['dist']));
 
 gulp.task('babel', () => {
   return gulp.src([
-      'src/js/**/*.js',
-      '!src/js/**/_*.js'
-      ])
+      'src/**/*.js',
+      '!src/**/_*.js'
+      ], {
+        base: 'src',
+        dot: true
+      })
+    .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('copy', () => {
   return gulp.src([
       'src/**/*',
-      '!src/js/**/*.js'
+      'src/**/_*.js',
+      '!src/**/*.js'
       ])
     .pipe(gulp.dest('dist'));
 });
