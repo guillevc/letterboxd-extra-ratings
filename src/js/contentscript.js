71dabfc05fb6ae5090d80162f123d25b017d1a2d
Guille <guillevch@gmail.com>
2016-04-21 18:06:07 +0200

    refactor

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,49 +1,5 @@
 'use strict';
 
-function getImdbId() {
-  const idRegExp = /.*\/title\/(.*)\/.*/;
-  const footerArr = document.getElementsByClassName('text-footer');
-  if (footerArr && footerArr.length > 0) {
-    const footer = footerArr[0];
-    if (footer && footer.firstElementChild && footer.firstElementChild.href) {
-      const imdbLink = footer.firstElementChild.href;
-      const idArr = idRegExp.exec(imdbLink);
-      if (idArr && idArr.length > 1) {
-        return idArr[1];
-      }
-    }
-  }
-  return null;
-}
-
-function buildApiUrl(imdbId) {
-  return `http://www.omdbapi.com/?i=${imdbId}&plot=short&r=json&tomatoes=true`;
-}
-
-function injectSection(imdbRating, tomatoRating) {
-  const sidebar = document.getElementsByClassName('sidebar')[0];
-
-  const newSection = document.createElement('section');
-  newSection.className = 'section';
-  newSection.style.marginTop = '10px';
-
-  const newSectionInnerHTML =
-   `<h2 class="section-heading">EXTERNAL RATINGS</h2>
-    <div class="text">
-      <p class="rating-green">
-        IMDb - ${imdbRating} out of 10
-        <span style="vertical-align: middle;" class="rating rated-${Math.round(imdbRating)}"></span>
-        <br />
-        Rotten Tomatoes - ${tomatoRating}
-        <span style="vertical-align: middle;" class="rating rated-${Math.round(tomatoRating)}"></span>
-      </p>
-    </div>`;
-
-  newSection.innerHTML = newSectionInnerHTML;
-  sidebar.appendChild(newSection);
-  sidebar.style.paddingBottom = '20px';
-}
-
 function makeRequest(method, url) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
@@ -60,12 +16,65 @@ function makeRequest(method, url) {
   });
 }
 
-const imdbId = getImdbId();
-const url = buildApiUrl(imdbId);
+function buildApiUrl(imdbId) {
+  return `http://www.omdbapi.com/?i=${imdbId}&plot=short&r=json&tomatoes=true`;
+}
 
-makeRequest('GET', url)
-  .then(rText => {
-    const parsed = JSON.parse(rText);
-    injectSection(parsed.imdbRating, parsed.tomatoRating);
-  })
-  .catch(rText => console.error('letterboxd-extra-ratings: error during api request ' + rText));
+function getImdbId() {
+  const idRegExp = /.*\/title\/(.*)\/.*/;
+  const footerArr = document.getElementsByClassName('text-footer');
+  if (footerArr && footerArr.length > 0) {
+    const footer = footerArr[0];
+    if (footer && footer.firstElementChild && footer.firstElementChild.href) {
+      const imdbLink = footer.firstElementChild.href;
+      const idArr = idRegExp.exec(imdbLink);
+      if (idArr && idArr.length > 1) {
+        return idArr[1];
+      }
+    }
+  }
+  return null;
+}
+
+
+function injectSection(imdbRating, tomatoRating) {
+  const sidebar = document.getElementsByClassName('sidebar')[0];
+
+  const newSection = document.createElement('section');
+  newSection.className = 'section';
+  newSection.style.marginTop = '10px';
+
+  const newSectionInnerHTML =
+   `<h2 class="section-heading">EXTERNAL RATINGS</h2>
+    <div class="text">
+      <p class="rating-green">
+        IMDb - ${imdbRating}
+        <span style="vertical-align: middle;" class="rating rated-${Math.round(imdbRating)}"></span>
+        <br />
+        Rotten Tomatoes - ${tomatoRating}
+        <span style="vertical-align: middle;" class="rating rated-${Math.round(tomatoRating)}"></span>
+      </p>
+    </div>`;
+
+  newSection.innerHTML = newSectionInnerHTML;
+  sidebar.appendChild(newSection);
+  sidebar.style.paddingBottom = '20px';
+}
+
+const imdbId = getImdbId();
+if (!imdbId) {
+  console.error('letterboxd-extra-ratings: imdb id not found in page');
+} else {
+  const url = buildApiUrl(imdbId);
+  makeRequest('GET', url)
+    .then(rText => {
+      let parsed;
+      try {
+        parsed = JSON.parse(rText);
+      } catch(e) {
+        console.error(`letterboxd-extra-ratings: ${e.message}`);
+      }
+      injectSection(parsed.imdbRating, parsed.tomatoRating);
+    })
+    .catch(rText => console.error('letterboxd-extra-ratings: error during api request ' + rText));
+}
